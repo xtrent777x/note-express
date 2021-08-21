cdf048f8f6147eb88106992db4ea9e784ffc0d41
Trent Roesbery <xtrent777x@gmail.com>
2021-08-21 15:37:52 -0600

    heroku error fix attempts

--- main.js
+++ main.js
@@ -4,8 +4,9 @@ const apiRoutes = require('./routes/apiRoutes');
 const htmlRoutes = require('./routes/htmlRoutes');
 
 
+
+const PORT = process.env.PORT || '8080'; // heroku port issue https://fredriccliver.medium.com/heroku-router-at-error-code-h10-desc-app-crashed-method-get-path-46da4cdce7ad
 const app = express ();
-const PORT = process.env.PORT || 3001; // heroku port issue https://fredriccliver.medium.com/heroku-router-at-error-code-h10-desc-app-crashed-method-get-path-46da4cdce7ad
 
 //middleware
 app.use(express.json());
